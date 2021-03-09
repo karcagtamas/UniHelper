406119ec57d54a8be5c2fcaea5a2894422e2afe2
karcagtamas <karcagtamas@outlook.com>
2021-03-09 22:31:14 +0100

    #7, #8, #9 Extend to Period and Subject

--- UniHelper/Shared/Dialogs/TaskDialog.razor.cs
+++ UniHelper/Shared/Dialogs/TaskDialog.razor.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
@@ -23,12 +25,22 @@ namespace UniHelper.Shared.Dialogs
 
         [Inject]
         private ISubjectTaskService SubjectTaskService { get; set; }
+        
+        [Inject]
+        private IPeriodService PeriodService { get; set; }
+        
+        [Inject]
+        private ISubjectService SubjectService { get; set; }
 
         private TaskType AddType { get; set; }
         private EditContext TaskContext { get; set; }
         private TaskModel Model { get; set; }
         private bool IsEdit { get; set; }
+        private int ParentSelectedId { get; set; }
         private int SelectedId { get; set; }
+        private List<PeriodDto> Periods { get; set; }
+        private List<SubjectDto> SourceSubjects { get; set; }
+        private List<SubjectDto> Subjects { get; set; }
 
         protected override Task OnInitializedAsync()
         {
@@ -47,60 +59,99 @@ namespace UniHelper.Shared.Dialogs
             return base.OnInitializedAsync();
         }
 
+        private async void TypeChanged(TaskType type)
+        {
+            Periods = new List<PeriodDto>();
+            SourceSubjects = new List<SubjectDto>();
+            switch (type)
+            {
+                case TaskType.Period:
+                    Periods = await PeriodService.GetList();
+                    break;
+                case TaskType.Subject:
+                    Periods = await PeriodService.GetList();
+                    SourceSubjects = await SubjectService.GetList();
+                    break;
+            }
+            AddType = type;
+            StateHasChanged();
+        }
+
+        private void ParentIdChanged(int id)
+        {
+            Subjects = new List<SubjectDto>();
+            if (SourceSubjects != null)
+            {
+                Subjects = SourceSubjects.Where(x => x.PeriodId == id).ToList();
+            }
+            ParentSelectedId = id;
+            StateHasChanged();
+        }
+
         private async void Save()
         {
-            if (TaskContext.Validate())
+            if ((AddType == TaskType.Period || AddType == TaskType.Subject) && SelectedId == 0)
             {
-                if (AddType == TaskType.Global)
+                return;
+            }
+
+            if (!TaskContext.Validate()) return;
+            
+            switch (AddType)
+            {
+                case TaskType.Global when IsEdit:
                 {
-                    if (IsEdit)
+                    if (await GlobalTaskService.Update(TaskData.Id, new GlobalTaskModel(Model)))
                     {
-                        if (await GlobalTaskService.Update(TaskData.Id, new GlobalTaskModel(Model)))
-                        {
-                            Dialog.Close(DialogResult.Ok(true));
-                        }
+                        Dialog.Close(DialogResult.Ok(true));
                     }
-                    else
-                    {
-                        if (await GlobalTaskService.Create(new GlobalTaskModel(Model)))
-                        {
-                            Dialog.Close(DialogResult.Ok(true));
-                        }
-                    }
+
+                    break;
                 }
-                else if (AddType == TaskType.Period)
+                case TaskType.Global:
                 {
-                    if (IsEdit)
+                    if (await GlobalTaskService.Create(new GlobalTaskModel(Model)))
                     {
-                        if (await PeriodTaskService.Update(TaskData.Id, new PeriodTaskModel(SelectedId, Model)))
-                        {
-                            Dialog.Close(DialogResult.Ok(true));
-                        }
+                        Dialog.Close(DialogResult.Ok(true));
                     }
-                    else
-                    {
-                        if (await PeriodTaskService.Create(new PeriodTaskModel(SelectedId, Model)))
-                        {
-                            Dialog.Close(DialogResult.Ok(true));
-                        }
-                    }
+
+                    break;
                 }
-                else if (AddType == TaskType.Subject)
+                case TaskType.Period when IsEdit:
                 {
-                    if (IsEdit)
+                    if (await PeriodTaskService.Update(TaskData.Id, new PeriodTaskModel(SelectedId, Model)))
                     {
-                        if (await SubjectTaskService.Update(TaskData.Id, new SubjectTaskModel(SelectedId, Model)))
-                        {
-                            Dialog.Close(DialogResult.Ok(true));
-                        }
+                        Dialog.Close(DialogResult.Ok(true));
                     }
-                    else
+
+                    break;
+                }
+                case TaskType.Period:
+                {
+                    if (await PeriodTaskService.Create(new PeriodTaskModel(SelectedId, Model)))
                     {
-                        if (await SubjectTaskService.Create(new SubjectTaskModel(SelectedId, Model)))
-                        {
-                            Dialog.Close(DialogResult.Ok(true));
-                        }
+                        Dialog.Close(DialogResult.Ok(true));
                     }
+
+                    break;
+                }
+                case TaskType.Subject when IsEdit:
+                {
+                    if (await SubjectTaskService.Update(TaskData.Id, new SubjectTaskModel(SelectedId, Model)))
+                    {
+                        Dialog.Close(DialogResult.Ok(true));
+                    }
+
+                    break;
+                }
+                case TaskType.Subject:
+                {
+                    if (await SubjectTaskService.Create(new SubjectTaskModel(SelectedId, Model)))
+                    {
+                        Dialog.Close(DialogResult.Ok(true));
+                    }
+
+                    break;
                 }
             }
         }
